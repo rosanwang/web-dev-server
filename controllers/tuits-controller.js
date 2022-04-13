8d389dd0e4f13d67a42f4ce1c777c58d53c59b2b
Rosan <wang.ruox@northeastern.edu>
2022-04-13 14:14:15 -0400

    Revert "connection with db"
    This reverts commit 498dbe050bbbe90877dde9a1723b639f6b9bc91d.

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -1,4 +1,5 @@
-import * as tuitsDao from "../tuits/tuits-dao.js";
+import posts from "./tuits.js";
+let tuits = posts;
 
 const createTuit = (req, res) => {
     const newTuit = req.body;
@@ -16,25 +17,19 @@ const createTuit = (req, res) => {
     res.json(newTuit);
 }
 
-const findAllTuits = async (req, res) => {
-    const tuits = await tuitsDao.findAllTuits()
-    res.json(tuits);
-}
+const findAllTuits = (req, res) => res.json(tuits);
 
-const updateTuit = async (req, res) => {
+const updateTuit = (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updatedTuit = req.body;
-    //uits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
-    //res.sendStatus(200);
-    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updatedTuit);
-    res.send(status);
+    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
+    res.sendStatus(200);
 }
 
-const deleteTuit = async (req, res) => {
+const deleteTuit = (req, res) => {
     const tuitdIdToDelete = req.params.tid;
-    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-    //tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
-    res.send(status);
+    tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
+    res.sendStatus(200);
 }
 
 export default (app) => {
